42d5252f748e47117032780ff74093b2b4152b3a
Mishell <53318144+MishellGomezCavero@users.noreply.github.com>
2021-08-07 03:53:33 -0500

    Update api.service.js

--- client/src/utils/api.service.js
+++ client/src/utils/api.service.js
@@ -1,31 +1,64 @@
 import axios from "axios";
 
-export default async message => {
-   try {
-      if(message.split(" ")[0] == "codigo"){
+export default async (message) => {
+  try {
+    if (message.split(" ")[0] == "codigo") {
       const { data } = await axios.post(
-         "https://tottus-chat-bot.azurewebsites.net/api/consultapedido",
-            { codigo: message.split(" ")[1] }
+        "https://tottus-chat-bot.azurewebsites.net/api/consultapedido",
+        { codigo: message.split(" ")[1] }
       );
       return {
-         user: "bot",
-         message: "estado del pedido: " + data.estado + " fecha entregada: " + data.fecha_entrega
+        user: "bot",
+        message:
+          "estado del pedido: " +
+          data.estado +
+          " fecha entregada: " +
+          data.fecha_entrega,
       };
-      }
-
+    }
+    if (message.split(" ")[0] == "codigo_p") {
       const { data } = await axios.post(
-        "https://tottus-chat-bot.azurewebsites.net/api/dialogflowbot?code=%2FBciBExoall7DU8h6hrJmbCWaRPIF1ZuqzhH65ipT6Az03J2e1QHgg%3D%3D",
-         { message }
+        "https://tottus-chat-bot.azurewebsites.net/api/ConsultaProducto?",
+        { codigo: message.split(" ")[1] }
       );
       return {
-         user: "bot",
-         message: data.fulfillmentText
+        user: "bot",
+        message:
+          "Cantidad: " +
+          data.cantidad_en_stock +
+          " Precio: " +
+          data.precio_venta,
       };
-   } catch (err) {
-      console.log(err);
+    }
+    if (message.split(" ")[0] == "dni") {
+      const { data } = await axios.post(
+        "https://tottus-chat-bot.azurewebsites.net/api/consultacliente",
+        { dni: message.split(" ")[1] }
+      );
       return {
-         user: "bot",
-         message: "Hey, ha ocurrido un error...."
+        user: "bot",
+        message:
+          "Nombre cliente: " +
+          data.nombre_cliente +
+          " Apellido cliente: " +
+          data.apellido_contacto +
+          " Linea direccion: " +
+          data.linea_direccion1,
       };
-   }
+    }
+    const { data } = await axios.post(
+      "https://tottus-chat-bot.azurewebsites.net/api/dialogflowbot?code=%2FBciBExoall7DU8h6hrJmbCWaRPIF1ZuqzhH65ipT6Az03J2e1QHgg%3D%3D",
+      { message }
+    );
+    return {
+      user: "bot",
+      message: data.fulfillmentText,
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      user: "bot",
+      message: "Hey, ha ocurrido un error....",
+    };
+  }
 };
